ae994e09d498747db139e50d4ff8599f5e6bed32
Varun Srivastava <varunsrivastava.v@gmail.com>
2024-01-16 22:32:52 +0000

    feat: add lplr supprt

--- shell_scripts/run_mixed_precision_finetune_gsm8k.fish
+++ shell_scripts/run_mixed_precision_finetune_gsm8k.fish
@@ -2,7 +2,7 @@
 
 set -x ACCELERATE_MIXED_PRECISION fp16; python examples/loftq_finetuning/train_gsm8k_llama.py \
     --model_name_or_path ./artifacts/loftq/Llama-2-7b-hf-4bit-64rank \
-    --output_dir exp_results/loftq/gsm8k/llama-2-7b-hf-4bit-64rank \
+    --output_dir exp_results/gsm8k/llama-2-7b-hf-4bit-64rank \
     --learning_rate 1e-4  \
     --weight_decay 0.1 \
     --lr_scheduler_type cosine \
